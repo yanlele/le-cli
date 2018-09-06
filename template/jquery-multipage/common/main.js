21aa817c23ee27b615de97c47843de40ad4f6221
yanlele <331393627@qq.com>
2018-09-06 13:37:10 +0800

    '整理完所有的handlebars扩展'

--- template/jquery-multipage/common/main.js
+++ template/jquery-multipage/common/main.js
@@ -1,3 +1,6 @@
 import './base.css';
-let setRem = require('./module/setRem');
+
+
+let setRem = require('./module/setRem');    // 移动端自动计算rem
+let helpers = require('./lib/handlebars-helper-extend/helper');   // 注册handlebars helpers
 setRem.init();
--- template/jquery-multipage/page/home_mobile/entry.js
+++ template/jquery-multipage/page/home_mobile/entry.js
@@ -1,9 +1,11 @@
 import('./home.css');
 let helloTemplate = require('../../template/hello.hbs');
 let helloTemplateComponent = helloTemplate({
-    name: 'yanle'
+    name: 'yanle',
+    a: 1,
+    b: 2
 });
 
-$('#container').html(helloTemplate);
+$('#container').html(helloTemplateComponent);
 
 console.log('home');
