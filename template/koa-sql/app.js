0865e87c19e440aad84cd714f9c64ce46d4c19f7
yanlele <331393627@qq.com>
2018-10-02 17:25:05 +0800

    [+]添加日志模板

--- template/koa-sql/app.js
+++ template/koa-sql/app.js
@@ -1,18 +1,18 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logs-middleware')
-const session = require('koa-session-minimal')
-const MysqlStore = require('koa-mysql-session')
-const config = require('./config')
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logs-middleware');
+const session = require('koa-session-minimal');
+const MysqlStore = require('koa-mysql-session');
+const config = require('./config');
 
-const routers = require('./routes/index')
+const routers = require('./routes/index');
 
 // 错误处理
-onerror(app)
+onerror(app);
 
 
 // session存储配置
@@ -21,18 +21,18 @@ const sessionMysqlConfig= {
     password: config.database.PASSWORD,
     database: config.database.DATABASE,
     host: config.database.HOST,
-}
+};
 
 // 配置session中间件
 app.use(session({
     key: 'USER_SID',
     store: new MysqlStore(sessionMysqlConfig)
-}))
+}));
 
 // 解析requestBody中间件
 app.use(bodyparser({
     enableTypes: ['json', 'form', 'text']
-}))
+}));
 app.use(json());
 
 // 加载日志
@@ -41,26 +41,26 @@ app.use(logger({
     applicationName: 'app'
 }));
 
-app.use(require('koa-static')(__dirname + '/views'))
+app.use(require('koa-static')(__dirname + '/views'));
 
 app.use(views(__dirname + '/views', {
     extension: 'html'
-}))
+}));
 
 // 日志
 app.use(async (ctx, next) => {
-    const start = new Date()
-    await next()
-    const ms = new Date() - start
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+});
 
 // 初始化路由中间件
-app.use(routers.routes()).use(routers.allowedMethods())
+app.use(routers.routes()).use(routers.allowedMethods());
 
 // 错误处理
 app.on('error', (err, ctx) => {
     console.error('server error', err, ctx)
 });
 
-module.exports = app
+module.exports = app;
