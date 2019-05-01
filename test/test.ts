54f6791dc40315156ef1a2d5738cfe5941eaea3c
yanlele <331393627@qq.com>
2019-05-01 14:28:57 +0800

    feat(add module ora):

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,5 @@
 import log from '../main/lib/log';
+import * as download from 'download-git-repo';
 import * as path from 'path';
 import * as userHome from 'user-home';
 
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,3 +3,4 @@ declare module 'inquirer';
 declare module 'commander';
 declare module 'chalk';
 declare module 'user-home';
+declare module 'download-git-repo';
